7f905075836aec328526890b2c98180ba01d28de
Mailis <mailis.kokla@gmail.com>
2016-10-17 15:16:57 +0300

    Files update

--- search.php
+++ search.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mailis
- * Date: 15/10/16
- * Time: 23:11
- */
 ?>
-
 <form action="<?php echo BASE;?>auth/search" method="post">
     <input type="text" name="bandname" placeholder="Band name" />
     <button type="submit">Search</button>
